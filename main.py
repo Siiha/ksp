b2acd7c5adc5c72a6408c9e0fcc1434467b7143b
Siiha <90766306+Siiha@users.noreply.github.com>
2025-05-10 20:49:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-#r10
-# Siina Hakala
 from collections import Counter
 t = "ksp"
-def tarkistus(pelaajan_valinta,koneen_valinta):
-    
-    if pelaajan_valinta==t[t.index(koneen_valinta)-1]: print("Voitit!"); x=1
-    elif pelaajan_valinta==koneen_valinta: print("Tasapeli!"); x=0
-    else: print("Hävisit!"); x=0
-    return x
-def historian_kapistely(pelaajan_valinta):
-	with open('historia.txt','r+') as f:
-          r = f.read().replace('\n','')
-          f.write(str(t.index(pelaajan_valinta)))
-          return Counter(r)
-def tallenna(pelaaja,kierokset):
-    with open('tulokset.txt','r+') as f:
-        f.write(f"{pelaaja['nimi']} voitot:{pelaaja['voitot']}/{kierokset}\n")
-    
-botin_valinta = lambda d: t[int(d.most_common()[0][0])-1]
-def main():
-    h = {"k":"""
+kadet = {"k":"""
     _______
 ---'   ____)
       (_____)
@@ -41,13 +22,28 @@ def main():
          _______)
 ---.__________)
 """} 
+def tarkistus(pelaajan_valinta,koneen_valinta):
+    if pelaajan_valinta==t[t.index(koneen_valinta)-1]: print("Voitit!"); x=1
+    elif pelaajan_valinta==koneen_valinta: print("Tasapeli!"); x=0
+    else: print("Hävisit!"); x=0
+    return x
+def historian_kapistely(pelaajan_valinta):
+	with open('historia.txt','r+') as f:
+          r = f.read().replace('\n','')
+          f.write(str(t.index(pelaajan_valinta)))
+          return Counter(r)
+def tallenna(pelaaja,kierokset):
+    with open('tulokset.txt','r+') as f:
+        f.write(f"{pelaaja['nimi']} voitot:{pelaaja['voitot']}/{kierokset}\n")  
+botin_valinta = lambda d: t[int(d.most_common()[0][0])-1]
+def main():
     pelaaja = {'nimi':input('Nimimerkkisi: '),'voitot': 0,'käsi':""};
     jatka = 1 
     kierokset = 0 
     while jatka:
         pelaaja['käsi']=input("Kätesi k:kivi s:sakset p:paperi: ")
         bot = botin_valinta(historian_kapistely(pelaaja['käsi']))
-        print(h[bot])
+        print(kadet[bot])
         pelaaja['voitot']+=tarkistus(pelaaja['käsi'],bot)
         jatka = int(input("Haluatko jatkaa 1: kyllä 0: ei: "))
         kierokset += 1
